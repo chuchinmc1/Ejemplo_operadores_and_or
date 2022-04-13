96246a24953b5473bb6c1c88df3522cdcad8cbc2
chuchinmc1 <chuchinmc@gmail.com>
2022-04-13 10:44:17 -0500

    Create ESP32-CAM_Operadores_and_or.ino

---
+++ ESP32-CAM_Operadores_and_or/ESP32-CAM_Operadores_and_or.ino
@@ -0,0 +1,32 @@
+/* Ejemplo Operadores and y or en C
+ * Además se utiliza el sensor de temperatura DHT11
+ * Por: JMC
+ * 13 de abril de 2022 
+ */ 
+
+// Bibliotecas
+
+
+// Constantes
+
+
+// Variables
+
+
+// Definicion de objetos
+
+// Condiciones iniciales
+void setup() {// Inicio Void setup()
+  Serial.begin(115200);
+  Serial.println("Programa de uso de los operadoradores: and y or");
+  delay(5000);
+}// Fin Void setup()
+
+// Cuerpo del programa
+void loop() { // Inicio void loop()
+
+
+    
+} //Fin loop()
+
+// Funciones del usuario
